65249e5b51f1e12522ede59dd7ceed0e7639f055
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-02-17 17:52:41 -0800

    Update fps.hx

--- fps.hx
+++ fps.hx
@@ -14,6 +14,8 @@ import openfl.events.Event;
 var game:FlxGame;
 //optimize this fuckin shit
 
+//put this in titlestate
+
 // Disable mouse smoothing for better performance
 Mouse.setNativeCursor(true);
 
@@ -21,6 +23,7 @@ Mouse.setNativeCursor(true);
 #if debug
   debug = false;
 #end
+
 
 // Set the frame rate to a lower value for better performance
 openfl.Lib.current.stage.frameRate = 30;
